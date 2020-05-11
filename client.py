d99aaa5518c8dcadea532fb36b56944f45b8046d
ScienceKot <56827517+ScienceKot@users.noreply.github.com>
2020-05-11 17:55:16 +0300

    Update client.py

--- client.py
+++ client.py
@@ -20,8 +20,11 @@ sock.connect((HOST, PORT))
 
 i = 0
 while True:
+    # Capturarea imaginii din video pentru fiecare frame
     ret, frame = video_capture.read()
+    # Decolorarea imaginilor
     gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    # Identificarea fiecărei fație prezentă în frame
     faces = faceCascade.detectMultiScale(
         gray,
         scaleFactor=1.1,
@@ -29,16 +32,22 @@ while True:
         minSize=(30, 30),
         flags = cv2.CASCADE_SCALE_IMAGE
     )
-    '''''
+    # Desenarea dreptunghiurilor în jurul fețelor
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 0)
+    # Prezentarea a ceea ce vede caluculatorul
     cv2.imshow("Video", frame)
-    '''
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+    # Inițializarea datelor pentru trimitere
     data = {"Time" : datetime.datetime.now().strftime("%Y : %M : %d - %H : %m"), 'Faces' : len(faces)}
+    # Convertirea dictionarului în format string
     data = json.dumps(data)
+    # Convertirea stringului în bytes format
     data = data.encode()
+    # Trimiterea datelor
     sock.send(data)
+    # Realozarea unei pauze de 2 sec
     time.sleep(2)
+# Închiderea conecțiunii TCP/IP
 sock.close()
